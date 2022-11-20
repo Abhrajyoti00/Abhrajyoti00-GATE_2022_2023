29f826cb438c4c6b30e2db9c8eb928cd38cfaac4
Abhrajyoti00 <abhrajyoti00@gmail.com>
2022-11-20 11:06:13 +0530

    Added inversions

--- Testing Programs on GATE/mergesort.cpp
+++ Testing Programs on GATE/mergesort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int a[10] = {10,12,14,16,18,8,9,10,13,15};
-int b[10];
+int b[10], inv = 0;
 void merge(int low, int mid, int high){
     int i = low, j = mid+1, k = low, p;
     while((i<=mid) && (j<=high)){
@@ -10,6 +10,7 @@ void merge(int low, int mid, int high){
             i++;
         }
         else{
+            inv+=mid-i+1;
             b[k] = a[j];
             j++;
         }
@@ -35,6 +36,7 @@ int main(){
     for(int i = 0; i<10; i++){
         cout<<a[i]<<", ";
     }
+    cout<<"\n The no of inversions = "<<inv;
     return 0;
 }
 
