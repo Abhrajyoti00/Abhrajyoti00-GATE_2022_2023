8901d34ae29c21e097f494e5426508bf8c32836f
Abhrajyoti00 <abhrajyoti00@gmail.com>
2023-01-09 18:55:09 +0530

    Analysing AIMT

--- Testing Programs on GATE/c.c
+++ Testing Programs on GATE/c.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// struct intarray {
+// int size;
+// int *data;
+// };
+// struct intarray *a;
+ 
+// void f (int n)
+// {
+//     a = malloc(sizeof(struct intarray));
+//     a->size = n;
+//     a->data = malloc(sizeof(int)*a->size);
+    
+// }
+struct s {
+    int x, y;
+};
+struct s sarr[20];
+
+
 int main (){
     int a,b,c;
     a=11.3;
     b=9.1;
     c=10;
-    printf("%d",a);
+    sarr[10].x = 20;
+    a = *((int *)((char *)sarr + 10 * sizeof(struct s)));
+    printf("%d \n",((struct s *)((char *)sarr + 10 * sizeof(struct s)))->x);
+    printf("%d\n",a);
     printf("%d ",b);
     printf("%d \n" ,c);
     printf("%c \n",11.2);
@@ -15,5 +37,8 @@ int main (){
     printf("%c", 128);
     printf("Doom");
     printf("%c", 256);
+    
+    
+
     return(0);
 }
